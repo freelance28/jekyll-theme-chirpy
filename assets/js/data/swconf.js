ff8c74466642f3e94619d38502c50c5084b5f29f
freelance28 <freelance28@users.noreply.github.com>
2025-04-15 15:59:23 +0000

    deploy: fa4937665a195cf0df2d1f3476323875829fa933

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1744732762',resources: [
+      '/jekyll-theme-chirpy/assets/css/jekyll-theme-chirpy.css',
+      '/jekyll-theme-chirpy/',
+      
+        '/jekyll-theme-chirpy/categories/',
+      
+        '/jekyll-theme-chirpy/tags/',
+      
+        '/jekyll-theme-chirpy/archives/',
+      
+        '/jekyll-theme-chirpy/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
